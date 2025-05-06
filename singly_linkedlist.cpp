a8947d6389511451d28511bca2e2f204dd5514d9
shafaasalsabila00 <sashaaraabil@gmail.com>
2025-05-06 11:19:54 +0700

    mengatur node

--- singly_linkedlist.cpp
+++ singly_linkedlist.cpp
@@ -40,5 +40,24 @@ public:
             return;
         }
 
+        Node *previous = START;
+        Node *current = START;
+
+        while ((current != NULL) && (nim >= current->noMhs))
+        {
+            if (nim == current->noMhs)
+            {
+                cout << "\nDuplikasi noMhs tidak diijinkan\n";
+                return;
+            }
+            previous = current;
+            current = current->next;
+        }
+
+        nodeBaru->next = current;
+        previous->next = nodeBaru;
+    }
+
+    
         
 };
