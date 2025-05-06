f2d6a31ee2ffdb2f5300cb44f30a7ee1e0c1e833
shafaasalsabila00 <sashaaraabil@gmail.com>
2025-05-06 12:18:14 +0700

    tampilkan cari data

--- singly_linkedlist.cpp
+++ singly_linkedlist.cpp
@@ -158,6 +158,34 @@ int main()
                          << "Data dengan nomor mahasiswa " << nim 
                          << " berhasil dihapus " << endl;
             }
+            break;
+            case '3':
+            {
+                mhs.traverse();
+            }
+            break;
+            case '4':
+            {
+                if (mhs.listEmpty() == true)
+                {
+                    cout << "\nList Kosong\n";
+                    break;
+                }
+                Node *previous, *current;
+                cout << endl
+                     << "Masukkan no mahasiswa yang dicari : ";
+                cin >> nim;
+                if (mhs.Search(nim,&previous, &current) == false)
+                    cout << endl
+                         << "Data tidak ditemukan" << endl;
+                else
+                {
+                    cout << endl
+                         << "Data tidak ditemukan" << endl;
+                    cout << "\nNo Mahasiswa: " << current->noMhs << endl;
+                    cout << "\n";
+                }
+            }
             
         }
 
