e227d2779db8fb8ebda08d9709df2a91bf4f36be
shafaasalsabila00 <sashaaraabil@gmail.com>
2025-05-06 12:13:53 +0700

    menambahkan case 1 dan 2

--- singly_linkedlist.cpp
+++ singly_linkedlist.cpp
@@ -131,5 +131,35 @@ int main()
              << "Masukkan pilihan (1-5): ";
         cin >> ch;
         
-        
+        switch (ch)
+        {
+            case '1':
+            {
+                mhs.addNode();
+            }
+            break;
+
+            case '2':
+            {
+                if (mhs.listEmpty())
+                {
+                    cout << endl
+                         << "List Kosong" << endl;
+                    break;
+                }
+                cout << endl
+                     << "\nMasukkan no mahasiswa yang akan dihapus : ";
+                cin >> nim;
+                if (mhs.delNode(nim) == false)
+                    cout << endl
+                         << "Data tidak ditemukan" << endl;
+                else
+                    cout << endl
+                         << "Data dengan nomor mahasiswa " << nim 
+                         << " berhasil dihapus " << endl;
+            }
+            
+        }
+
+    }
 }
