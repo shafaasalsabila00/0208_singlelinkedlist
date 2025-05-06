324398007073b870fafd77ed45aaf7ce5c17c8dc
shafaasalsabila00 <sashaaraabil@gmail.com>
2025-05-06 11:25:02 +0700

    menambahkan fungsi search

--- singly_linkedlist.cpp
+++ singly_linkedlist.cpp
@@ -63,6 +63,20 @@ public:
         return (START == NULL);
     }
 
+    bool Search(int nim, Node **previous, Node **current)
+    {
+        *previous = START;
+        *current = START;
+
+        while ((*current != NULL) && (nim != (*current)->noMhs))
+        {
+            *previous = *current;
+            *current = (*current)->next;
+        }
+
+        return (*current != NULL);
+    }
+
     
 
         
